b92c78e773b54cf0680a89379cc3aba122b0437d
violetbaggins <violetbaggins@gmail.com>
2021-04-20 20:30:29 -0300

    change db

--- html_css/src/database/config/config.js
+++ html_css/src/database/config/config.js
@@ -8,17 +8,17 @@ module.exports = {
     "operatorsAliases": false
   },
   "test": {
-    "username": "freedbtech_violetbaggins2",
+    "username": "freedbtech_violetbaggins",
     "password": "123456789",
-    "database": "freedbtech_violetbaggins_db2",
+    "database": "freedbtech_mundoparlantedb",
     "host": "freedb.tech",
     "dialect": "mysql",
     "operatorsAliases": false
   },
   "production": {
-    "username": "freedbtech_violetbaggins2",
+    "username": "freedbtech_violetbaggins",
     "password": "123456789",
-    "database": "freedbtech_violetbaggins_db2",
+    "database": "freedbtech_mundoparlantedb",
     "host": "freedb.tech",
     "dialect": "mysql",
     "operatorsAliases": false
